01cd456ff0803dbe8790d21e96a04b38d90569eb
simoganger <simonsoft7@gmail.com>
2019-12-25 00:01:43 +0100

    refactor: Renaming earliest_birth_year to earliest_b_year

--- bikeshare.py
+++ bikeshare.py
@@ -174,11 +174,11 @@ def user_stats(df):
         print(gender)
 
         # display earliest, most recent, and most common year of birth
-        earliest_birth_year = df['Birth Year'].min()
+        earliest_b_year = df['Birth Year'].min()
         most_recent_birth_year = df['Birth Year'].max()
         most_common_birth_year = df['Birth Year'].mode()[0]
         
-        print("The earliest Birth Year : ", earliest_birth_year)
+        print("The earliest Birth Year : ", earliest_b_year)
         print("The most recent Birth Year : ", most_recent_birth_year)
         print("The most common Birth Year : ", most_common_birth_year)
     except:
