7fdcaed7fe39b6f87f9b444f7526a8546e9b4938
simoganger <simonsoft7@gmail.com>
2019-12-24 23:46:02 +0100

    docs: Modify the second docstring of the bikeshare.py at line 40

--- bikeshare.py
+++ bikeshare.py
@@ -37,7 +37,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day or all the months and days available.
 
     Args:
         (str) city - name of the city to analyze
