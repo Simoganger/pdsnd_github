cdff6d6af3202880941f09c2944195a5df38df49
simoganger <simonsoft7@gmail.com>
2019-12-25 00:13:07 +0100

    refactor: Renaming most_common_birth_year to most_common_b_year

--- bikeshare.py
+++ bikeshare.py
@@ -176,11 +176,11 @@ def user_stats(df):
         # display earliest, most recent, and most common year of birth
         earliest_birth_year = df['Birth Year'].min()
         most_recent_birth_year = df['Birth Year'].max()
-        most_common_birth_year = df['Birth Year'].mode()[0]
+        most_common_b_year = df['Birth Year'].mode()[0]
         
         print("The earliest Birth Year : ", earliest_birth_year)
         print("The most recent Birth Year : ", most_recent_birth_year)
-        print("The most common Birth Year : ", most_common_birth_year)
+        print("The most common Birth Year : ", most_common_b_year)
     except:
         print("Sorry no statistics availaible for the columns 'Gender' and 'Birth Year' on this data file ! ")
 
